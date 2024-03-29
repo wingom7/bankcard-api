911df919fa30ff212ae55edb7e71462db3ee32d9
darwgom7 <darwgom7@gmail.com>
2024-03-28 23:40:50 -0500

    Initial commit

---
+++ src/main/java/com/wingom/bankcardapi/BankcardApiApplication.java
@@ -0,0 +1,13 @@
+package com.wingom.bankcardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankcardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankcardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wingom/bankcardapi/BankcardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wingom.bankcardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankcardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
